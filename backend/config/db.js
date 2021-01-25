59ec30b871e822d1ddd7948ab043094a63ec2c33
Aushtiin <aotangban@gmail.com>
2021-01-25 23:16:01 +0100

    initialized redux

---
+++ backend/config/db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const db = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+
+        console.log(`MongoDB connected: ${conn.connection.host}`.green.underline)
+    } catch (error) {
+        console.error(`Error: ${error.message}`.red.bold)
+        process.exit(1)
+    }
+}
+
+export default db
