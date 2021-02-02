86c91306f0164f6eb4e0ee8d416648b4b06ee4c4
Aushtiin <aotangban@gmail.com>
2021-02-02 23:12:50 +0100

    added register screen

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -21,12 +21,65 @@ const authUser = asyncHandler(async (req, res) => {
     }
 });
 
-const getUserProfile = asyncHandler(async (req, res) => {
-    const user = await User.findById(req.user._id) 
-    
-    if (user){
-        
+const registerUser = asyncHandler(async (req, res) => {
+    const { email, password, name } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+        res.status(400)
+        throw new Error('User already exists')
+    }
+
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid User Data')
     }
 });
 
-export { authUser, getUserProfile }
+const getUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+});
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    const user = await User.findById(req.user._id)
+
+    if (user) {
+        user.name = req.body.name || user.name;
+        user.email = req.body.email || user.email;
+        if(req.body.password) {
+            user.password = req.body.password;
+        }
+    } else {
+        res.status(404)
+        throw new Error('User not found')
+    }
+});
+
+export { authUser, getUserProfile, registerUser, updateUserProfile }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,9 @@ import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import CartScreen from './screens/CartScreen';
-import { BrowserRouter as Router, Route} from 'react-router-dom'
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 const App = () => {
   return (
@@ -12,9 +14,11 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <Route exact path='/' component={HomeScreen} />
           <Route path='/product/:id' component={ProductScreen} />
+          <Route path='/login' component={LoginScreen} />
+          <Route path='/register' component={RegisterScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route exact path='/' component={HomeScreen} />
         </Container>
       </main>
       <Footer />
